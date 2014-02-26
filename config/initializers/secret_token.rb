d82470d04fae0c920bf3bb7f977876951628ba65
Rachel Werner <rachelannwerner@gmail.com>
2014-02-25 23:25:37 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GirlGeekLibrary::Application.config.secret_key_base = '205809b8e49f10864558550c85580751de5646bd05721ae76ae1ed72f0a7e2242c74866cac81b059ee204f1ddabe3668cf4a43fd541197fc4106c666aae68292'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GirlGeekLibrary::Application.config.session_store :cookie_store, key: '_GirlGeekLibrary_session'
